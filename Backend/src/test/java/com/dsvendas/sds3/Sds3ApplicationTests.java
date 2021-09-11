377217e05abfe75814c600c031a4fa8c4f8971f7
Juliano Gomes <julianocg2013@gmail.com>
2021-09-11 00:42:19 -0300

    Criacao de projetos

---
+++ Backend/src/test/java/com/dsvendas/sds3/Sds3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsvendas.sds3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sds3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
